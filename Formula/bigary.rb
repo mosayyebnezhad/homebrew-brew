e107612ad9ed3633df37b7abad1ba2213fdd7b5c
Mohammad <59848315+mosayyebnezhad@users.noreply.github.com>
2025-08-11 11:27:46 +0330

    Update bigary.rb

--- Formula/bigary.rb
+++ Formula/bigary.rb
@@ -2,7 +2,7 @@ class Bigary < Formula
   desc "A simple CLI tool"
   homepage "https://github.com/mosayyebnezhad/bigary"
   url "https://github.com/mosayyebnezhad/bigary/releases/download/cv0.0.0/bigary"
-  sha256 "aef694c645f491d931bca6410ae89ce052d5d6f70725386bcc6a8fc6f27766b0"
+  sha256 "07ec94f73183b6b412754c295cba15aba1f5821e535d9bc4fa542497bb26f087"
   version "0.0.1"
 
   def install
