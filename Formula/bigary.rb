5e34ec69cc1728607bf2c4fc0f0ddf5e91475051
mosayyebnezhad <mohammadmosayyebnezhad@gmail.com>
2025-08-11 11:15:50 +0330

    change bianry

--- Formula/bigary.rb
+++ Formula/bigary.rb
@@ -2,7 +2,7 @@ class Bigary < Formula
   desc "A simple CLI tool"
   homepage "https://github.com/mosayyebnezhad/bigary"
   url "https://github.com/mosayyebnezhad/bigary/releases/download/cv0.0.0/bigary"
-  sha256 "87036e621bc4ba6443542f022a737188452b47757bd6c171dede5023c46534fa"
+  sha256 "6329a75b93a91300198176c8137fc412c98322f8530fe7aaa20dbbebf5dffdb4"
   version "0.0.1"
 
   def install
